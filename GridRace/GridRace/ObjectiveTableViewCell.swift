96499906425f372976bbb4e9328dd30ea07599db
Unknown <campbell.graham@gridstone.com.au>
2018-02-26 17:03:28 +1100

    File for objective custom table view cell added

---
+++ GridRace/GridRace/ObjectiveTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  ObjectiveTableViewCell.swift
+//  GridRace
+//
+//  Created by Campbell Graham on 26/2/18.
+//  Copyright © 2018 Gridstone. All rights reserved.
+//
+
+import UIKit
+
+class ObjectiveTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
