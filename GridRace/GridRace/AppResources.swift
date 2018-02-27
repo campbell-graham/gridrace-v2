90259b02f0c9ab860e7d791ac38096aa5de7c262
Unknown <campbell.graham@gridstone.com.au>
2018-02-27 13:31:33 +1100

    Color changes

--- GridRace/GridRace/AppResources.swift
+++ GridRace/GridRace/AppResources.swift
@@ -10,10 +10,10 @@ import UIKit
 
 
 struct AppColors {
-    static var textPrimaryColor = #colorLiteral(red: 0.9503886421, green: 0.9503886421, blue: 0.9503886421, alpha: 1)
-    static var textSecondaryColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
     static var cellColor = #colorLiteral(red: 0.2039215686, green: 0.2470588235, blue: 0.2941176471, alpha: 1)
     static var backgroundColor = #colorLiteral(red: 0.1647058824, green: 0.1960784314, blue: 0.2352941176, alpha: 1)
-    static var greenHighlightColor = #colorLiteral(red: 0.07450980392, green: 0.8078431373, blue: 0.4, alpha: 1)
-    static var starPointsColor = #colorLiteral(red: 0.9176470588, green: 1, blue: 0.3607843137, alpha: 1)
+    static var textPrimaryColor = #colorLiteral(red: 0.926295042, green: 0.9409481287, blue: 0.9453216195, alpha: 1)
+    static var textSecondaryColor = #colorLiteral(red: 0.7391201854, green: 0.7641983628, blue: 0.7814642787, alpha: 1)
+    static var greenHighlightColor = #colorLiteral(red: 0.1803921569, green: 0.8, blue: 0.4431372549, alpha: 1)
+    static var starPointsColor = #colorLiteral(red: 0.9450980392, green: 0.768627451, blue: 0.05882352941, alpha: 1)
 }
