cc0bf2b324cb48992ed2e1178f05233bb4ba2ec0
Unknown <christian.vassallo@gridstone.com.au>
2018-03-01 12:02:16 +1100

    added textFieldController to handle text answers
    - changed textfield on details screen to a uilabel
    - changed textView on detail screen to be non editable
    - renamed starImageView to PointBorderImageView

---
+++ GridRace/GridRace/ContainerView.swift
@@ -0,0 +1,39 @@
+//
+//  textFieldView.swift
+//  GridRace
+//
+//  Created by Christian on 2/28/18.
+//  Copyright © 2018 Gridstone. All rights reserved.
+//
+
+import UIKit
+
+class ContainerView: UIView {
+
+    let textLabel = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        textLabel.textAlignment = .center
+        textLabel.backgroundColor = AppColors.cellColor
+        textLabel.textColor = AppColors.textPrimaryColor
+        textLabel.font = UIFont.boldSystemFont(ofSize: 16)
+
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(textLabel)
+
+        NSLayoutConstraint.activate([
+            textLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            textLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            textLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            textLabel.heightAnchor.constraint(equalToConstant: 44)
+        ])
+
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
--- GridRace/GridRace/DetailViewController.swift
+++ GridRace/GridRace/DetailViewController.swift
@@ -26,7 +26,7 @@ class DetailViewController: UIViewController {
 
     private let mapView = MKMapView()
     private let descLabel = UITextView()
-    private let starImageView = UIImageView()
+    private let pointBorderImageView = UIImageView()
     private let pointLabel = UILabel()
     private let answerView: UIView
     private let interactImageView = UIImageView()
@@ -40,7 +40,7 @@ class DetailViewController: UIViewController {
         case 0: // imageview
             answerView = UIImageView()
         case 1: // textField
-            answerView = textFieldView()
+            answerView = ContainerView()
         case 2: // pin view
             answerView = UIView()
         default:
@@ -60,7 +60,7 @@ class DetailViewController: UIViewController {
         case 0: // imageview
             answerView = UIImageView()
         case 1: // textField
-            answerView = textFieldView()
+            answerView = ContainerView()
         case 2: // pin view
             answerView = UIView()
         default:
@@ -80,9 +80,7 @@ class DetailViewController: UIViewController {
 
         super.viewDidLoad()
 
-        navigationController?.navigationBar.prefersLargeTitles = true
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .done, target: self, action: #selector(cancelHandler) )
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Get Clue", style: .done, target: self, action: #selector(clueButtonHandler) )
+        navigationController?.navigationBar.prefersLargeTitles = false
         title = objective.name
 
         initialiseViews()
@@ -95,6 +93,7 @@ class DetailViewController: UIViewController {
         view.backgroundColor = AppColors.backgroundColor
         descLabel.textColor = AppColors.textPrimaryColor
         pointLabel.textColor = AppColors.greenHighlightColor
+        pointBorderImageView.tintColor = AppColors.greenHighlightColor
         interactImageView.tintColor = AppColors.greenHighlightColor
         hintImageView.tintColor = AppColors.greenHighlightColor
 
@@ -103,9 +102,9 @@ class DetailViewController: UIViewController {
         // misc stuff
         descLabel.backgroundColor = AppColors.backgroundColor
         descLabel.font = UIFont.systemFont(ofSize: 16)
+        descLabel.isEditable = false
 
-        starImageView.contentMode = .scaleAspectFit
-        starImageView.tintColor = AppColors.greenHighlightColor
+        pointBorderImageView.contentMode = .scaleAspectFit
 
         pointLabel.font = UIFont.boldSystemFont(ofSize: 16)
 
@@ -114,7 +113,7 @@ class DetailViewController: UIViewController {
         switch answerView {
         case is UIImageView :
             interactGestureRecogniser = UITapGestureRecognizer(target: self, action: #selector(selectPhoto))
-        case is textFieldView :
+        case is ContainerView :
             interactGestureRecogniser = UITapGestureRecognizer(target: self, action: #selector(enterAnswer))
         default:
             interactGestureRecogniser = UITapGestureRecognizer(target: self, action: nil)
@@ -133,57 +132,53 @@ class DetailViewController: UIViewController {
     private func updateViewsData() {
 
         descLabel.text = objective.desc
-        starImageView.image = #imageLiteral(resourceName: "circle")
+        pointBorderImageView.image = #imageLiteral(resourceName: "circle")
         pointLabel.text = "\(objective.pointsCount)"
+        interactImageView.image = answerView is UIImageView ? #imageLiteral(resourceName: "camera") : #imageLiteral(resourceName: "textCursor")
+        hintImageView.image = #imageLiteral(resourceName: "hint")
     }
 
     private func setUpLayout() {
 
-        for view in [ mapView, descLabel, starImageView, answerView] {
+        for view in [ mapView, descLabel, pointBorderImageView, answerView, interactImageView, hintImageView] {
             view.translatesAutoresizingMaskIntoConstraints = false
             self.view.addSubview(view)
         }
 
         pointLabel.translatesAutoresizingMaskIntoConstraints = false
-        starImageView.addSubview(pointLabel)
+        pointBorderImageView.addSubview(pointLabel)
 
         var constraints = ([
             mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            mapView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25),
+            mapView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2),
 
             descLabel.topAnchor.constraint(equalTo: mapView.bottomAnchor, constant: 16),
-            descLabel.leadingAnchor.constraint(equalTo: starImageView.trailingAnchor, constant: 16),
+            descLabel.leadingAnchor.constraint(equalTo: pointBorderImageView.trailingAnchor, constant: 16),
             descLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            descLabel.heightAnchor.constraint(equalToConstant: 160),
+            descLabel.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2),
 
-            starImageView.topAnchor.constraint(equalTo: descLabel.topAnchor),
-            starImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            starImageView.heightAnchor.constraint(equalToConstant: 64),
-            starImageView.widthAnchor.constraint(equalToConstant: 64),
+            pointBorderImageView.topAnchor.constraint(equalTo: descLabel.topAnchor),
+            pointBorderImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            pointBorderImageView.heightAnchor.constraint(equalToConstant: 64),
+            pointBorderImageView.widthAnchor.constraint(equalToConstant: 64),
 
-            pointLabel.centerXAnchor.constraint(equalTo: starImageView.centerXAnchor),
-            pointLabel.centerYAnchor.constraint(equalTo: starImageView.centerYAnchor),
-
-
-            // TODO: add interactImageView Constraints
-            // TODO: add hintImageView Constraints
-            // remove get clue from nav bar
-
+            pointLabel.centerXAnchor.constraint(equalTo: pointBorderImageView.centerXAnchor),
+            pointLabel.centerYAnchor.constraint(equalTo: pointBorderImageView.centerYAnchor),
         ])
 
         switch answerView {
         case is UIImageView:
             constraints += [
-                answerView.topAnchor.constraint(greaterThanOrEqualTo: starImageView.bottomAnchor, constant: 16),
+                answerView.topAnchor.constraint(greaterThanOrEqualTo: descLabel.bottomAnchor, constant: 16),
                 answerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                answerView.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -32),
-                answerView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+                answerView.bottomAnchor.constraint(lessThanOrEqualTo: interactImageView.bottomAnchor, constant: -16),
+                answerView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.3),
             answerView.heightAnchor.constraint(equalTo: answerView.widthAnchor)]
-        case is textFieldView:
+        case is ContainerView:
             constraints += [
-                answerView.topAnchor.constraint(equalTo: starImageView.bottomAnchor),
+                answerView.topAnchor.constraint(equalTo: descLabel.bottomAnchor),
                 answerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
                 answerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
                 answerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)]
@@ -191,12 +186,21 @@ class DetailViewController: UIViewController {
             break
         }
 
+        constraints += [
+            interactImageView.topAnchor.constraint(greaterThanOrEqualTo: answerView.bottomAnchor, constant: 16),
+            interactImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -(view.center.x / 2) ),
+            interactImageView.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
+            interactImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.20),
+            interactImageView.heightAnchor.constraint(equalTo: interactImageView.widthAnchor),
+
+            hintImageView.topAnchor.constraint(equalTo: interactImageView.topAnchor),
+            hintImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: (view.center.x / 2) ),
+            hintImageView.bottomAnchor.constraint(equalTo: interactImageView.bottomAnchor),
+            hintImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.20),
+            hintImageView.heightAnchor.constraint(equalTo: hintImageView.widthAnchor)
+        ]
+
         NSLayoutConstraint.activate(constraints)
-    }
-
-    @objc private func cancelHandler() {
-
-        navigationController?.popViewController(animated: true)
     }
 
     @objc private func clueButtonHandler() {
@@ -234,6 +238,30 @@ class DetailViewController: UIViewController {
 
     @objc func enterAnswer() {
 
+        let textFieldViewController = TextFieldViewController(closure: enterAnswerCompletion)
+        textFieldViewController.modalTransitionStyle = .crossDissolve
+        textFieldViewController.modalPresentationStyle = .overCurrentContext
+        present(textFieldViewController, animated: true, completion: nil)
+    }
+
+    func enterAnswerCompletion(answer: String) {
+
+        if let answerView = answerView as? ContainerView {
+            answerView.textLabel.text = answer
+            playHudAnimation()
+        }
+    }
+
+    private func playHudAnimation() {
+
+        let hudView = HudView.hud(inView: navigationController!.view, animated: true)
+        hudView.text = "Complete!"
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0,
+          execute: {
+            hudView.hide()
+            //self.navigationController?.popViewController(animated: true)
+        })
     }
 
     //set textView to be scrolled to top
@@ -292,18 +320,6 @@ UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
 
         dismiss(animated: true, completion: nil)
-    }
-
-    private func playHudAnimation() {
-
-        let hudView = HudView.hud(inView: navigationController!.view, animated: true)
-            hudView.text = "Complete!"
-
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0,
-        execute: {
-            hudView.hide()
-            //self.navigationController?.popViewController(animated: true)
-        })
     }
 
 }
