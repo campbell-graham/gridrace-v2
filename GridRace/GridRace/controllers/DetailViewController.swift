448f6a189696ac13df0db5d67c24dcc2dea82e44
Unknown <christian.vassallo@gridstone.com.au>
2018-03-01 17:23:59 +1100

    Started Collapsable ViewController animations
    - need to convert code from objc to swift

--- GridRace/GridRace/controllers/DetailViewController.swift
+++ GridRace/GridRace/controllers/DetailViewController.swift
@@ -13,6 +13,7 @@ class DetailViewController: UIViewController {
 
     var objective: Objective
     private let mapView = MKMapView()
+    private let collapsableDetailsView = UIView()
     private let descLabel = UITextView()
     private let pointBorderImageView = UIImageView()
     private let pointLabel = UILabel()
@@ -20,6 +21,8 @@ class DetailViewController: UIViewController {
     private let interactImageView = UIImageView()
     private let hintImageView = UIImageView()
     private let pointDeductionValue = 2
+    private var isCollapsed = false
+    private var collapsableDetailsAnimator: UIViewPropertyAnimator?
 
     init(objective: Objective) {
 
@@ -33,7 +36,7 @@ class DetailViewController: UIViewController {
         case .password: // pin view
             answerView = UIView()
         default:
-            answerView = UIView()
+            break
 
         }
 
@@ -59,7 +62,7 @@ class DetailViewController: UIViewController {
     private func initialiseViews() {
 
         //Colors
-        view.backgroundColor = AppColors.backgroundColor
+        collapsableDetailsView.backgroundColor = AppColors.backgroundColor
         descLabel.textColor = AppColors.textPrimaryColor
         pointLabel.textColor = AppColors.greenHighlightColor
         pointBorderImageView.tintColor = AppColors.greenHighlightColor
@@ -73,6 +76,9 @@ class DetailViewController: UIViewController {
         descLabel.font = UIFont.systemFont(ofSize: 16)
         descLabel.isEditable = false
 
+        let collapseGestureRecogniser = UIPanGestureRecognizer(target: self, action: #selector(collapseAnimationHandler))
+        pointBorderImageView.addGestureRecognizer(collapseGestureRecogniser)
+        pointBorderImageView.isUserInteractionEnabled = true
         pointBorderImageView.contentMode = .scaleAspectFit
 
         pointLabel.font = UIFont.boldSystemFont(ofSize: 16)
@@ -109,9 +115,14 @@ class DetailViewController: UIViewController {
 
     private func setUpLayout() {
 
-        for view in [ mapView, descLabel, pointBorderImageView, answerView, interactImageView, hintImageView] {
+        for view in [ mapView, collapsableDetailsView] {
             view.translatesAutoresizingMaskIntoConstraints = false
             self.view.addSubview(view)
+        }
+
+        for view in [descLabel, pointBorderImageView, answerView, interactImageView, hintImageView] {
+            view.translatesAutoresizingMaskIntoConstraints = false
+            collapsableDetailsView.addSubview(view)
         }
 
         pointLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -121,15 +132,20 @@ class DetailViewController: UIViewController {
             mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            mapView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2),
+            mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
 
-            descLabel.topAnchor.constraint(equalTo: mapView.bottomAnchor, constant: 16),
+            collapsableDetailsView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: (view.bounds.height) / 4 ),
+            collapsableDetailsView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collapsableDetailsView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collapsableDetailsView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+
+            descLabel.topAnchor.constraint(equalTo: collapsableDetailsView.topAnchor, constant: 16),
             descLabel.leadingAnchor.constraint(equalTo: pointBorderImageView.trailingAnchor, constant: 16),
-            descLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            descLabel.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2),
+            descLabel.trailingAnchor.constraint(equalTo: collapsableDetailsView.trailingAnchor, constant: -16),
+            descLabel.heightAnchor.constraint(equalTo: collapsableDetailsView.heightAnchor, multiplier: 0.2),
 
             pointBorderImageView.topAnchor.constraint(equalTo: descLabel.topAnchor),
-            pointBorderImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            pointBorderImageView.leadingAnchor.constraint(equalTo: collapsableDetailsView.leadingAnchor, constant: 16),
             pointBorderImageView.heightAnchor.constraint(equalToConstant: 64),
             pointBorderImageView.widthAnchor.constraint(equalToConstant: 64),
 
@@ -141,16 +157,16 @@ class DetailViewController: UIViewController {
         case is UIImageView:
             constraints += [
                 answerView.topAnchor.constraint(greaterThanOrEqualTo: descLabel.bottomAnchor, constant: 16),
-                answerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                answerView.centerXAnchor.constraint(equalTo: collapsableDetailsView.centerXAnchor),
                 answerView.bottomAnchor.constraint(lessThanOrEqualTo: interactImageView.bottomAnchor, constant: -16),
-                answerView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.3),
+                answerView.widthAnchor.constraint(equalTo: collapsableDetailsView.widthAnchor, multiplier: 0.3),
             answerView.heightAnchor.constraint(equalTo: answerView.widthAnchor)]
         case is ContainerView:
             constraints += [
                 answerView.topAnchor.constraint(equalTo: descLabel.bottomAnchor),
-                answerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                answerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-                answerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)]
+                answerView.leadingAnchor.constraint(equalTo: collapsableDetailsView.leadingAnchor),
+                answerView.trailingAnchor.constraint(equalTo: collapsableDetailsView.trailingAnchor),
+                answerView.bottomAnchor.constraint(equalTo: collapsableDetailsView.safeAreaLayoutGuide.bottomAnchor)]
         default:
             break
         }
@@ -158,18 +174,140 @@ class DetailViewController: UIViewController {
         constraints += [
             interactImageView.topAnchor.constraint(greaterThanOrEqualTo: answerView.bottomAnchor, constant: 16),
             interactImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -(view.center.x / 2) ),
-            interactImageView.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
-            interactImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.20),
+            interactImageView.bottomAnchor.constraint(lessThanOrEqualTo: collapsableDetailsView.safeAreaLayoutGuide.bottomAnchor, constant: -16),
+            interactImageView.widthAnchor.constraint(equalTo: collapsableDetailsView.widthAnchor, multiplier: 0.20),
             interactImageView.heightAnchor.constraint(equalTo: interactImageView.widthAnchor),
 
             hintImageView.topAnchor.constraint(equalTo: interactImageView.topAnchor),
-            hintImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: (view.center.x / 2) ),
+            hintImageView.centerXAnchor.constraint(equalTo: collapsableDetailsView.centerXAnchor, constant: (view.center.x / 2) ),
             hintImageView.bottomAnchor.constraint(equalTo: interactImageView.bottomAnchor),
-            hintImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.20),
+            hintImageView.widthAnchor.constraint(equalTo: collapsableDetailsView.widthAnchor, multiplier: 0.20),
             hintImageView.heightAnchor.constraint(equalTo: hintImageView.widthAnchor)
         ]
 
         NSLayoutConstraint.activate(constraints)
+    }
+
+    @objc private func collapseAnimationHandler() {
+
+
+
+        let translation = [recognizer translationInView:self.view.superview];
+
+        if (recognizer.state == UIGestureRecognizerStateBegan)
+        {
+            onPanBegin()
+        }
+        if (recognizer.state == UIGestureRecognizerStateEnded)
+        {
+            let velocity = [recognizer velocityInView:self.view];
+            panningEndedWithTranslation:(translation: translation, velocity: velocity)
+        }
+        else
+        {
+            CGPoint translation = [recognizer translationInView:self.view.superview];
+            panningChangedWithTranslation(translation: velocity)
+        }
+    }
+
+    private func onPanBegin() {
+
+        if self.collapsableDetailsAnimator.isRunning {
+            return
+        }
+
+        var targetFrame: CGRect
+
+        if !isCollapsed {
+
+            isCollapsed = true
+
+            let newY = self.view.bounds.height - self.tabBarController!.tabBar.bounds.height - self.pointBorderImageView.bounds.height - 32
+            let oldFrame = self.collapsableDetailsView.frame
+            targetFrame = CGRect(x: oldFrame.minX, y: newY, width: oldFrame.size.width, height: oldFrame.size.height)
+
+        } else {
+
+            let bounds = self.collapsableDetailsView.bounds
+            let newY = UIApplication.shared.statusBarFrame.height + self.navigationController!.navigationBar.bounds.height + ((self.view.bounds.height) / 4) + 1
+            targetFrame = CGRect(x: bounds.minX, y: newY, width: bounds.width, height: bounds.height)
+
+            isCollapsed = false
+        }
+
+        collapsableDetailsAnimator = UIViewPropertyAnimator(duration: 0.6, curve: .easeIn,
+                                                            animations: {
+                                                                self.collapsableDetailsView.frame = targetFrame
+        })
+
+    }
+
+    private func panningChangedWithTranslation(translation: CGPoint)
+    {
+        if (self.playerViewAnimator.isRunning)
+        {
+        return;
+        }
+
+        CGFloat translatedY = self.view.center.y + translation.y;
+
+        CGFloat progress;
+        switch (self.playerState) {
+        case PlayerStateThumbnail:
+        progress = 1 - (translatedY / self.view.center.y);
+        break;
+        case PlayerStateFullscreen:
+        progress = (translatedY / self.view.center.y) - 1;
+        }
+
+        progress = MAX(0.001, MIN(0.999, progress));
+
+        self.playerViewAnimator.fractionComplete = progress;
+    }
+
+    private func panningEndedWithTranslation:(translation: CGPoint, velocity: CGPoint)
+    {
+        self.panGestureRecognizer.enabled = NO;
+
+        CGFloat screenHeight = [[UIScreen mainScreen] bounds].size.height;
+        __weak ViewController *weakSelf = self;
+
+        switch (self.playerState) {
+        case PlayerStateThumbnail:
+            if (translation.y <= -screenHeight / 3 || velocity.y <= -100)
+            {
+                self.playerViewAnimator.reversed = NO;
+                [self.playerViewAnimator addCompletion:^(UIViewAnimatingPosition finalPosition) {
+                weakSelf.playerState = PlayerStateFullscreen;
+                weakSelf.panGestureRecognizer.enabled = YES;
+                }];
+                }
+                else
+                {
+                self.playerViewAnimator.reversed = YES;
+                [self.playerViewAnimator addCompletion:^(UIViewAnimatingPosition finalPosition) {
+                weakSelf.playerState = PlayerStateThumbnail;
+                weakSelf.panGestureRecognizer.enabled = YES;
+                }];
+            }
+            break;
+        case PlayerStateFullscreen:
+            if (translation.y >= screenHeight / 3 || velocity.y >= 100)
+            {
+                self.playerViewAnimator.reversed = NO;
+                [self.playerViewAnimator addCompletion:^(UIViewAnimatingPosition finalPosition) {
+                weakSelf.playerState = PlayerStateThumbnail;
+                weakSelf.panGestureRecognizer.enabled = YES;
+                }];
+            }
+            else
+            {
+                self.playerViewAnimator.reversed = YES;
+                [self.playerViewAnimator addCompletion:^(UIViewAnimatingPosition finalPosition) {
+                weakSelf.playerState = PlayerStateFullscreen;
+                weakSelf.panGestureRecognizer.enabled = YES;
+                }];
+            }
     }
 
     @objc private func clueButtonHandler() {
@@ -285,7 +423,7 @@ UIImagePickerControllerDelegate, UINavigationControllerDelegate {
         dismiss(animated: true, completion: nil)
 
         playHudAnimation()
-        ObjectiveManager.shared.completeObjectives.insert(self.objective.id) 
+        ObjectiveManager.shared.completeObjectives.insert(self.objective.id)
     }
 
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
