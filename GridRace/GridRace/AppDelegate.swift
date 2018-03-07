df9ec1d720de5c6ed62d54abf7af96cf0deb9042
Unknown <campbell.graham@gridstone.com.au>
2018-03-07 16:59:27 +1100

    Start screen only presented on first launch

--- GridRace/GridRace/AppDelegate.swift
+++ GridRace/GridRace/AppDelegate.swift
@@ -19,46 +19,45 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        //create date if first time launching
-        if !UserDefaults.standard.bool(forKey: "HasLaunchedOnce") {
-            UserDefaults.standard.set(true, forKey: "HasLaunchedOnce")
-            //add date object to user defaults
-            UserDefaults.standard.set(Date(), forKey: "FirstLaunchDate")
-        }
-        
-        _ = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {_ in
-            let interval = Date().timeIntervalSince(AppResources.firstLaunchDate)
-            
-            let ti = NSInteger(interval)
-            
-            let seconds = ti % 60
-            let minutes = (ti / 60) % 60
-            let hours = ti / 3600
-            
-            AppResources.timeToDisplay = NSString(format: "%0.2d:%0.2d:%0.2d", hours, minutes, seconds) as String
-            self.timerView.timeLabel.text = AppResources.timeToDisplay
-        })
-        
         //firebase setup
         FirebaseApp.configure()
         
         window = UIWindow(frame: UIScreen.main.bounds)
         
-        let placesViewController = ObjectiveTableViewController(title: "Places", tabBarImage: #imageLiteral(resourceName: "directional_arrow"), dataCategory: ObjectiveCategory(rawValue: "places"), objectives: &AppResources.placesObjectives, data: &AppResources.placesUserData)
-        let bonusViewController = ObjectiveTableViewController(title: "Bonus", tabBarImage: #imageLiteral(resourceName: "clock_outline"), dataCategory: .bonus, objectives: &AppResources.bonusObjectives, data: &AppResources.bonusUserData)
+        //create date if first time launching
+        if !UserDefaults.standard.bool(forKey: "HasLaunchedOnce") {
+            UserDefaults.standard.set(true, forKey: "HasLaunchedOnce")
+            window?.rootViewController = StartScreenViewController()
+        } else {
+            _ = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {_ in
+                let interval = Date().timeIntervalSince(AppResources.firstLaunchDate)
+                
+                let ti = NSInteger(interval)
+                
+                let seconds = ti % 60
+                let minutes = (ti / 60) % 60
+                let hours = ti / 3600
+                
+                AppResources.timeToDisplay = NSString(format: "%0.2d:%0.2d:%0.2d", hours, minutes, seconds) as String
+                self.timerView.timeLabel.text = AppResources.timeToDisplay
+            })
+            
+            let placesViewController = ObjectiveTableViewController(title: "Places", tabBarImage: #imageLiteral(resourceName: "directional_arrow"), dataCategory: ObjectiveCategory(rawValue: "places"), objectives: &AppResources.placesObjectives, data: &AppResources.placesUserData)
+            let bonusViewController = ObjectiveTableViewController(title: "Bonus", tabBarImage: #imageLiteral(resourceName: "clock_outline"), dataCategory: .bonus, objectives: &AppResources.bonusObjectives, data: &AppResources.bonusUserData)
+            
+            let mainTabController = UITabBarController()
+            
+            mainTabController.tabBar.barTintColor = AppColors.backgroundColor
+            mainTabController.tabBar.tintColor = AppColors.textPrimaryColor
+            
+            mainTabController.viewControllers = [UINavigationController(rootViewController: placesViewController), UINavigationController(rootViewController: bonusViewController)]
+            
+            addTimerToTabBar(tabBar: mainTabController.tabBar)
+            
+            window?.rootViewController = mainTabController
+            
+        }
         
-        let mainTabController = UITabBarController()
-        
-        mainTabController.tabBar.barTintColor = AppColors.backgroundColor
-        mainTabController.tabBar.tintColor = AppColors.textPrimaryColor
-        
-        
-
-        mainTabController.viewControllers = [UINavigationController(rootViewController: placesViewController), UINavigationController(rootViewController: bonusViewController)]
-        
-        addTimerToTabBar(tabBar: mainTabController.tabBar)
-        
-        window?.rootViewController = StartScreenViewController()
         
         
         //global styling
--- GridRace/GridRace/controllers/StartScreenViewController.swift
+++ GridRace/GridRace/controllers/StartScreenViewController.swift
@@ -12,6 +12,7 @@ class StartScreenViewController: UIViewController {
     
     var startButton = UIButton()
     var backgroundImage = UIImageView()
+    let timerView = TimerView()
     
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -23,6 +24,7 @@ class StartScreenViewController: UIViewController {
         startButton.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
         startButton.layer.cornerRadius = 10
         startButton.alpha = 0
+        startButton.addTarget(self, action: #selector(startRace), for: .touchUpInside)
         
         //image set up
         backgroundImage.image = #imageLiteral(resourceName: "gridrace_splash-min")
@@ -65,6 +67,51 @@ class StartScreenViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func startRace() {
+        //add date object to user defaults
+        UserDefaults.standard.set(Date(), forKey: "FirstLaunchDate")
+        
+        _ = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {_ in
+            let interval = Date().timeIntervalSince(AppResources.firstLaunchDate)
+            
+            let ti = NSInteger(interval)
+            
+            let seconds = ti % 60
+            let minutes = (ti / 60) % 60
+            let hours = ti / 3600
+            
+            AppResources.timeToDisplay = NSString(format: "%0.2d:%0.2d:%0.2d", hours, minutes, seconds) as String
+            self.timerView.timeLabel.text = AppResources.timeToDisplay
+        })
+        
+        let placesViewController = ObjectiveTableViewController(title: "Places", tabBarImage: #imageLiteral(resourceName: "directional_arrow"), dataCategory: ObjectiveCategory(rawValue: "places"), objectives: &AppResources.placesObjectives, data: &AppResources.placesUserData)
+        let bonusViewController = ObjectiveTableViewController(title: "Bonus", tabBarImage: #imageLiteral(resourceName: "clock_outline"), dataCategory: .bonus, objectives: &AppResources.bonusObjectives, data: &AppResources.bonusUserData)
+        
+        let mainTabController = UITabBarController()
+        
+        mainTabController.tabBar.barTintColor = AppColors.backgroundColor
+        mainTabController.tabBar.tintColor = AppColors.textPrimaryColor
+        
+        mainTabController.viewControllers = [UINavigationController(rootViewController: placesViewController), UINavigationController(rootViewController: bonusViewController)]
+        
+        addTimerToTabBar(tabBar: mainTabController.tabBar)
+        
+        present(mainTabController, animated: true, completion: nil)
+    }
+    
+    func addTimerToTabBar(tabBar: UITabBar) {
+        tabBar.addSubview(timerView)
+        
+        timerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            timerView.centerXAnchor.constraint (equalTo: tabBar.centerXAnchor),
+            timerView.bottomAnchor.constraint(equalTo: tabBar.bottomAnchor),
+            timerView.heightAnchor.constraint(equalToConstant: tabBar.bounds.height),
+            timerView.widthAnchor.constraint(equalToConstant: tabBar.frame.width * 0.3)
+            ])
     }
 
     override func didReceiveMemoryWarning() {
